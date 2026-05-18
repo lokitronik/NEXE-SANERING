bb83266dc8530eff9801b855a2090647d28ca15b
lokitronik <90688498+lokitronik@users.noreply.github.com>
2026-05-18 12:27:20 +0200

    Update Personligatillhörigheter.tsx

--- src/pages/services/Personligatillhörigheter.tsx
+++ src/pages/services/Personligatillhörigheter.tsx
@@ -8,7 +8,7 @@ const fadeInUp = {
   transition: { duration: 0.8, ease: [0.25, 1, 0.5, 1] }
 };
 
-export default function atillhörigheter() {
+export default function atillhörigheter() { 
   return (
     <motion.div
       initial={{ opacity: 0 }}
